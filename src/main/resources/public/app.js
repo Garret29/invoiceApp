19fc84d6e53c34053e1b8d42b1ce7f10f112c54e
Garret29 <szymon.piotrowski93@gmail.com>
2017-09-01 16:32:21 +0200

    commit

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -1,6 +1,15 @@
 const app = angular.module('invoiceApp', []);
 
 app.controller('controller', function ($scope) {
+    $scope.getRandomColor = function () {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
     $scope.invoice =
         {
             invoiceCity:" ",
