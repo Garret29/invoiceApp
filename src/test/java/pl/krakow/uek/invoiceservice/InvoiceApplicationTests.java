8371c1f371f1e460827430a1c9912313ace6d28b
Garret29 <szymon.piotrowski93@gmail.com>
2019-03-18 16:36:43 +0100

    fixed security vulnerabilities

--- src/test/java/pl/krakow/uek/invoiceservice/InvoiceApplicationTests.java
+++ src/test/java/pl/krakow/uek/invoiceservice/InvoiceApplicationTests.java
@@ -2,15 +2,13 @@ package pl.krakow.uek.invoiceservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import pl.krakow.uek.invoiceservice.service.PDFGenerationService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InvoiceApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
